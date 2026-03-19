7e102d53ce31a7a49100b1d46c7f3ef96ec87dc7
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-03-19 20:20:46 +0800

    Edit AddCommand.java to remove PREFIX_TAG

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -28,15 +28,12 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
             + PREFIX_MEMBERSHIP_EXPIRY_DATE + "EXPIRY_DATE "
-            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_MEMBERSHIP_EXPIRY_DATE + "2026-12-31 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_MEMBERSHIP_EXPIRY_DATE + "2026-12-31 ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
