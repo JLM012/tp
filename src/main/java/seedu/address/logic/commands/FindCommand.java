8b12cf7c21f8d8452e345ef9ce063e2972b94a59
SUN_HEQIAO <e1355839@u.nus.edu>
2026-04-04 16:22:55 +0800

    Add logging for FindCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -4,7 +4,9 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.ListUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -37,6 +39,8 @@ public class FindCommand extends Command {
 
     private final Predicate<Person> predicate;
 
+    private static final Logger logger = LogsCenter.getLogger(FindCommand.class);
+
     public FindCommand(Predicate<Person> predicate) {
         this.predicate = predicate;
     }
@@ -45,13 +49,18 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
 
+        logger.info("Executing FindCommand: ");
+
         List<Person> beforeFind = ListUtil.copyDisplayedList(model);
         model.updateFilteredPersonList(predicate);
         List<Person> afterFind = ListUtil.copyDisplayedList(model);
 
         if (ListUtil.isSameList(beforeFind, afterFind)) {
+            logger.warning("Found the same persons. No change in displayed list");
             return new CommandResult(Messages.MESSAGE_NO_CHANGE_IN_DISPLAYED_LIST);
         }
+
+        logger.warning("Successfully found persons matching criteria");
 
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
