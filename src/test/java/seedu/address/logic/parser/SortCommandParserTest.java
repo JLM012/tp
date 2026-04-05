af9e2fab062222dd045301fb855114b630fdf0f3
吉麦 <jimai@jimaideMacBook-Pro.local>
2026-04-05 13:38:10 +0800

    Re-arrange tests.

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -16,33 +16,58 @@ public class SortCommandParserTest {
     private final SortCommandParser parser = new SortCommandParser();
 
     @Test
-    public void parse_invalidArgs_returnsParseException() {
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPrefix_throwsParseException() {
         // No prefix
         assertParseFailure(parser, " desc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        // Multiple different prefixes
-        assertParseFailure(parser, " n/asc id/desc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        // Multiple identical prefixes
-        assertParseFailure(parser, " n/asc n/desc", getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
-        // Invalid order
-        assertParseFailure(parser, " n/invalid", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
         // Unsupported prefix
         assertParseFailure(parser, " x/asc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_validArgs_returnsSortCommand() {
+    public void parse_multiplePrefixes_throwsParseException() {
+        // Multiple different prefixes
+        assertParseFailure(parser, " n/asc id/desc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        // Multiple identical prefixes
+        assertParseFailure(parser, " n/asc n/desc", getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
+    }
+
+    @Test
+    public void parse_invalidOrder_throwsParseException() {
+        // Invalid order
+        assertParseFailure(parser, " n/invalid", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        // None (with prefix)
+        assertParseFailure(parser, " n/none", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_none_returnsSortCommand() {
+        SortCommand expectedNoPrefixNone = new SortCommand(null, "none", "none");
+        assertParseSuccess(parser, " none", expectedNoPrefixNone);
+        assertParseSuccess(parser, "\t none \n", expectedNoPrefixNone);
+    }
+
+    @Test
+    public void parse_validPrefix_returnsSortCommand() {
         // Ascending
         SortCommand expectedAsc = new SortCommand(
                 PersonComparators.NAME_ASC, "n/", "asc");
         assertParseSuccess(parser, " n/asc", expectedAsc);
+
         // Descending
         SortCommand expectedDesc = new SortCommand(
                 PersonComparators.NAME_DESC, "n/", "desc");
         assertParseSuccess(parser, " n/desc", expectedDesc);
-        // None (restore)
-        SortCommand expectedNone = new SortCommand(
-                null, "n/", "none");
-        assertParseSuccess(parser, " n/none", expectedNone);
+    }
+
+    @Test
+    public void parse_whitespace_returnsSortCommand() {
+        SortCommand expectedAsc = new SortCommand(
+                PersonComparators.NAME_ASC, "n/", "asc");
 
         // Leading space
         assertParseSuccess(parser, "  n/asc", expectedAsc);
@@ -50,7 +75,12 @@ public class SortCommandParserTest {
         assertParseSuccess(parser, " n/asc ", expectedAsc);
         // Both leading and trailing space
         assertParseSuccess(parser, "  n/asc ", expectedAsc);
-        // Multiple prefixes (should throw ParseException due to verifyNoDuplicatePrefixesFor)
-        assertParseFailure(parser, " n/asc n/desc", getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
+
+        // Tabs and newlines
+        assertParseSuccess(parser, "\t n/asc", expectedAsc);
+        assertParseSuccess(parser, " n/asc\n", expectedAsc);
+        assertParseSuccess(parser, " n/\nasc", expectedAsc);
+        assertParseSuccess(parser, " n/\tasc", expectedAsc);
+        assertParseSuccess(parser, "\t\n n/asc \n\t", expectedAsc);
     }
 }
